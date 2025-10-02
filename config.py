7cc4e46effe493bec7c4b7ab3ecaa18161339975
Koa Wells <kekoa.wells@concordia.ca>
2025-10-01 22:50:17 -0400

    Add .config variables for email and email password, add checks to validate .config variables, and update requirements.txt

---
+++ config.py
@@ -0,0 +1,54 @@
+"""
+Class for managing chain-link configuration variables.
+Code contributors: Koa Wells koa.wells@hotmail.com
+"""
+from pathlib import Path
+from pydantic_settings import BaseSettings
+from pydantic import field_validator
+
+class Settings(BaseSettings):
+  """
+  Global application configuration settings.
+  """
+  DISCORD_TOKEN: str = ''
+  EMAIL: str = ''
+  EMAIL_PASSWORD: str = ''
+
+  class Config:
+    env_file = Path("./.config").resolve()
+
+    @field_validator("DISCORD_TOKEN")
+    @classmethod
+    def check_discord_token(cls, value: str) -> str:
+      """
+      Checks to see if the DISCORD_TOKEN environment variable is set.
+      :param value: value of DISCORD_TOKEN
+      :return: str
+      """
+      if not value:
+        raise ValueError("DISCORD_TOKEN must be set")
+      return value
+
+    @field_validator("EMAIL")
+    @classmethod
+    def check_email(cls, value: str) -> str:
+      """
+      Checks to see if the EMAIL environment variable is set.
+      :param value: value of EMAIL
+      :return: str
+      """
+      if not value:
+        raise ValueError("EMAIL must be set")
+      return value
+
+    @field_validator("EMAIL_PASSWORD")
+    @classmethod
+    def check_email_password(cls, value: str) -> str:
+      """
+      Checks to see if the EMAIL_PASSWORD environment variable is set.
+      :param value: value of EMAIL_PASSWORD
+      :return: str
+      """
+      if not value:
+        raise ValueError("EMAIL_PASSWORD must be set")
+      return value
